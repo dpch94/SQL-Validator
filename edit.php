892f36c745ab455f85a5fba9b6338220d3fd9f2f
Devi Prasad Ilapavuluri <dpch94@gmail.com>
2022-01-03 23:10:07 +0100

    fix

--- edit.php
+++ edit.php
@@ -57,7 +57,7 @@ include "logic.php";
             </div> -->
 
             <div>
-                <button type="update" name="addpost-btn"class="btn btn-edit">Update</button>
+                <button type="update" name="update"class="btn btn-edit">Update</button>
             </div>
             
         </form>
@@ -88,7 +88,7 @@ include "logic.php";
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-<script src="js/scripts.js"></script>
+<!--script src="js/scripts.js"></script>-->
 
 
 
