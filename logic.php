f38286cf3cfa5dbf7c00da58ef0e342ded3dfe1a
Devi Prasad Ilapavuluri <dpch94@gmail.com>
2022-02-17 23:50:09 +0100

    update

--- logic.php
+++ logic.php
@@ -41,7 +41,6 @@ function createTables()
                                 `description` longblob NOT NULL,
                                 `image` longblob NOT NULL,
                                 `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-                                `rid` int(11) NOT NULL,
                                 `ratename` varchar(255) NOT NULL,
                                 `rating` float DEFAULT 0 )");
 
